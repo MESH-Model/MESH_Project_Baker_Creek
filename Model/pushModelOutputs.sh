e56a4f0ac5d188ea3766554862e5c33d5fff36e9
Haley Brauner <haley.brauner@usask.ca>
2019-10-21 15:17:40 -0600

    Add Input and Output folders to Model folder

---
+++ Model/pushModelOutputs.sh
@@ -0,0 +1,13 @@
+# rsync -avirh data.dat andrew@water-ami.usask.ca:~/Desktop/ImaginaryRepo/.
+rsync -avirh Scenario_1/Output/ hbrauner@graham.computecanada.ca:~/projects/rpp-hwheater/hbrauner/BakerCreekProject/Model/Scenario_1/Output
+
+rsync -avirh Scenario_2/Output/ hbrauner@graham.computecanada.ca:~/projects/rpp-hwheater/hbrauner/BakerCreekProject/Model/Scenario_2/Output
+
+#~/OneDrive/GitProjects/GitHub/MESH_Project_Baker_Creek/Model/
+
+#rsync -avirh ./Scenario_3/Output/ hbrauner@graham.computecanada.ca:~/projects/rpp-hwheater/hbrauner/BakerCreekProject/Model/Scenario_3/Output
+
+#rsync -avirh ./Scenario_2/Output/ hbrauner@graham.computecanada.ca:~/projects/rpp-hwheater/hbrauner/BakerCreekProject/Model/Scenario_2/Output
+
+#rsync -avirh ./Scenario_1/Output/ ~/OneDrive/GitProjects/GitHub/MESH_Project_Baker_Creek/Model/Test
+#Add a "-n" or "--dry-run: at the end of the flags to make it a dry run
