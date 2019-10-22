642f9647f4518e950ecbeada5ee7a50713126428
Haley Brauner <haley.brauner@usask.ca>
2019-10-22 12:57:20 -0600

    Update Readme and pushModelOutputs.sh files

---
+++ Model/pullModelOutputs.sh
@@ -0,0 +1,20 @@
+# This script pulls the model output file contents from Graham the local machine.
+# To use, change "hbrauner" to your username, and update the file paths
+# Note: each iteration will require you to enter your Graham password unless you've connected your ssh key to Graham.
+
+# Enter your model scenario numbers / names here (ensure you have a consistent format for the "scenario" name following)
+nums='1 2 3 1-P 2-P'
+
+for scenario_num in $nums
+do
+  scenario="Scenario_${scenario_num}"
+
+#Switch between dry run and regular by commenting/uncommenting the lines below
+
+#Dry Run
+rsync -avirh --dry-run hbrauner@graham.computecanada.ca:~/projects/rpp-hwheater/hbrauner/BakerCreekProject/Model/$scenario/Output/ $scenario/Output
+
+#Regular
+#rsync -avirh hbrauner@graham.computecanada.ca:~/projects/rpp-hwheater/hbrauner/BakerCreekProject/Model/$scenario/Output/ $scenario/Output
+
+done
