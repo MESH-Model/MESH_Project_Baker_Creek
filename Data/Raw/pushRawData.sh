9052e59292cb6610d35a0e01e2546e9b7e3a25b1
Haley Brauner <haley.brauner@usask.ca>
2019-10-22 10:18:48 -0600

    Add ReadMe file, pullRawData.sh, and update pushRawData.sh

--- Data/Raw/pushRawData.sh
+++ Data/Raw/pushRawData.sh
@@ -1,1 +1,1 @@
-rsync -avirh --dry-run ./ hbrauner@graham.computecanada.ca:~/projects/rpp-hwheater/hbrauner/BakerCreekProject/Data/Raw
+rsync -avirh ./ hbrauner@graham.computecanada.ca:~/projects/rpp-hwheater/hbrauner/BakerCreekProject/Data/Raw
