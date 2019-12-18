0221d761a82eb99b5b6ed8b748fd6f83297e9159
Haley Brauner <haley.brauner@usask.ca>
2019-12-18 15:06:47 -0600

    pullRawData.sh update

--- Data/Raw/pullRawData.sh
+++ Data/Raw/pullRawData.sh
@@ -1,5 +1,5 @@
 # Pull from Graham
-rsync -rultvzi hbrauner@graham.computecanada.ca:~/projects/rpp-hwheater/hbrauner/MESH_Project_Baker_Creek/Data/Raw/ .
+# rsync -rultvzi hbrauner@graham.computecanada.ca:~/projects/rpp-hwheater/hbrauner/MESH_Project_Baker_Creek/Data/Raw/ .
 
 # Pull from water-ami server
 rsync -rultvzi mws_students@water-ami.usask.ca:~/Haley/BakerCreekRawData/ .
