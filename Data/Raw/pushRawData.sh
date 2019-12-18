ea6b6df12b047c935bb5cf914bcf9b6f1fe10539
Haley Brauner <haley.brauner@usask.ca>
2019-12-18 15:05:21 -0600

    updated push and pull raw data scripts

--- Data/Raw/pushRawData.sh
+++ Data/Raw/pushRawData.sh
@@ -1,1 +1,5 @@
-rsync -rultvzi ./ hbrauner@graham.computecanada.ca:~/projects/rpp-hwheater/hbrauner/MESH_Project_Baker_Creek/Data/Raw
+# Push to Graham
+#rsync -rultvzi ./ hbrauner@graham.computecanada.ca:~/projects/rpp-hwheater/hbrauner/MESH_Project_Baker_Creek/Data/Raw
+
+# Push to water-ami server
+rsync -rultvzi ./ mws_students@water-ami.usask.ca:~/Haley/BakerCreekRawData
